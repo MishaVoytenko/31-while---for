526ce989cdeed4259a76edc92f8664c85b46c578
Misha Voytenko <minyavoytenko@gmail.com>
2022-12-10 11:41:20 +0200

    end norm4

--- norm3.js
+++ norm3.js
@@ -8,11 +8,19 @@ let odd = 0
 for (let i = 1; i <= 3; i++) {
   // nums[i - 1] = prompt("введіть число");
   const num = +prompt("введіть число");
-  if (num < 0) negative++
-  if (num > 0) positive++
-  if (num == 0) zeros++
-  if (num % 2 == 0) even++
-  if (num % 2 != 0) odd++
+
+  // if (num < 0) negative++
+  // else if (num > 0) positive++
+  // else zeros++
+
+  // if (num % 2) odd++
+  // else even++
+
+  negative += num < 0
+  positive += num > 0
+  zeros += num == 0
+  even += !(num % 2)
+  odd += num % 2
 
 }
 console.log({ positive, negative, zeros, even, odd })
---
+++ norm4.js
@@ -0,0 +1,7 @@
+let week = ['понеділок', 'вівторок', 'середа', 'четверг', 'пятниця', 'субота', 'неділя']
+console.log(week)
+
+for (let i = 0; i < week.length; i++) {
+  // console.log(week[i])
+  alert(week[i])
+}
